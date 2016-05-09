77adc0d596f429d080e30231e1d782f43f10b7b7
gadi gomez <gadi gomez>
2016-05-09 14:42:24 +0300

    develop first commit

--- app/src/main/java/com/example/nofit/summary/LogInActivity.java
+++ app/src/main/java/com/example/nofit/summary/LogInActivity.java
@@ -7,7 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+     
 import com.example.nofit.summary.model.Model;
 import com.example.nofit.summary.model.ModelFirebase;
 import com.example.nofit.summary.model.Student;
