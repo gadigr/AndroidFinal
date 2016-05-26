193802e9fe677a57ec348e9eae625edf29e759c5
gadi gomez <gadi gomez>
2016-05-26 15:57:01 +0300

    changed login signup and welcome screen styles

--- app/src/main/java/com/finalproject/kg/summary/LogInActivity.java
+++ app/src/main/java/com/finalproject/kg/summary/LogInActivity.java
@@ -1,7 +1,9 @@
 package com.finalproject.kg.summary;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.finalproject.kg.summary.model.Model;
 import com.finalproject.kg.summary.model.Student;
@@ -27,25 +30,30 @@ public class LogInActivity extends ActionBarActivity {
         final EditText etEmail = (EditText) findViewById(R.id.etEmail);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
         final ProgressBar pbLoading = (ProgressBar) findViewById(R.id.pbLoading);
+        final ProgressDialog dialog = new ProgressDialog(LogInActivity.this);
+        dialog.setMessage("Logging in...");
 
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                pbLoading.setVisibility(View.VISIBLE);
+                dialog.show();
                 Model.instance().login(etEmail.getText().toString(), etPassword.getText().toString(), new Model.SignupListener() {
                     @Override
                     public void success(AuthData authData) {
                         Log.d("TAG", "login OK");
+
                         Intent intent = new Intent(getApplicationContext(), MainNotesActivity.class);
                         startActivity(intent);
-                        pbLoading.setVisibility(View.GONE);
+                        dialog.dismiss();
                     }
 
                     @Override
                     public void fail(String msg) {
                         Log.d("TAG", "login FAIL " + msg);
-                        pbLoading.setVisibility(View.GONE);
+                        dialog.dismiss();
+                        Toast toast = Toast.makeText(context, "Login Failed: " + msg, Toast.LENGTH_LONG);
+                        toast.show();
                     }
                 });
 /*
